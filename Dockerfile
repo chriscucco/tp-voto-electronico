ee5c97433a4804a04c590c02ac13635577b5afb7
Christian cucco <cdc-96@hotmail.com>
2022-05-29 16:06:30 -0300

    add router

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 # syntax=docker/dockerfile:1
 
 FROM node:16.15.0
-ENV NODE_ENV=production
 
-WORKDIR /app
+WORKDIR /usr/src/app
 
 COPY package*.json ./
+
 RUN npm install
 
-CMD [ "npm", "start" ] 
+COPY . .
+
+EXPOSE 8000
+
+CMD [ "node", "index.js" ] 
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+const { createUser } = require('./controllers/users/users');
 const express = require('express')
 const app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-const { createUser } = require('./controllers/users/users');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Constants
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.post('/create_user', createUser)
+router.get("/",function(req,res){
+  res.send("Hello World1!");
+});
 
-app.listen(8000, () => {
+router.get("/home",function(req,res){
+  res.send("Hello World2");
+});
+
+app.use(express.static(path));
+app.use("/", router);
+
+app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
-});
+})
