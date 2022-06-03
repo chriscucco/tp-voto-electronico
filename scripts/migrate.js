7a3bf15e7c9f727bdbc696a6470ea8f3365cb4f8
Christian cucco <cdc-96@hotmail.com>
2022-06-02 21:05:45 -0300

    add sql connection and dummy get/post

---
+++ scripts/migrate.js
@@ -0,0 +1,18 @@
+const db = require('../dao/db')
+
+;(async () => {
+  try {
+    await db.schema.dropTableIfExists('users')
+    await db.schema.withSchema('public').createTable('users', (table) => {
+      table.increments()
+      table.string('user_id')
+      table.string('name')
+      table.string('last_name')
+    })
+    console.log('Created users table!')
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+})()
--- server/server.js
+++ server/server.js
@@ -4,20 +4,29 @@ const app = express();
 var path = require("path");
 
 var router = express.Router();
+const database = require('../dao/db')
 
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 app.use("/", express.static(path.join(__dirname, "..", "build")));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 router.use(function (req, res, next) {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/home",function(req,res){
-  res.send("Hello World!");
+router.get("/users", async(req,res) => {
+  const users = await database.select().from('users')
+  res.json(users)
+});
+
+router.post("/user", async (req, res) => {
+  const user = await database('users').insert({user_id: req.body.user_id, name: req.body.name, last_name: req.body.last_name }).returning('*')
+  res.json(user)
 });
 
 app.use(router);
