8f3a4659963a38129ee08a33edce0f6166308ff6
Christian cucco <cdc-96@hotmail.com>
2022-06-12 20:51:35 -0300

    add logs

--- dao/db.js
+++ dao/db.js
@@ -2,4 +2,9 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile')[environment];
 const knex = require('knex')
+console.log("////////////")
+console.log(config)
+console.log(process.env.NODE_ENV)
+console.log(process.env.DATABASE_URL)
+console.log("////////////")
 module.exports = knex(config);
