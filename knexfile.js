30e1d32173cf273d2ef95075ad4ee93031a87a3e
Christian cucco <cdc-96@hotmail.com>
2022-06-12 21:13:50 -0300

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,10 @@ module.exports = {
       },
     production: {
         client: 'postgres',
-        connection: process.env.DATABASE_URL,
+        connection: { 
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        },
         migrations: {
             directory: __dirname + '/scripts',
           },
