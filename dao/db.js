48a49a90cd8d71ae63723daeb5dc42096f526f57
Christian cucco <cdc-96@hotmail.com>
2022-06-12 21:19:08 -0300

    finish db config

--- dao/db.js
+++ dao/db.js
@@ -2,11 +2,4 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile')[environment];
 const knex = require('knex')
-console.log("////////////")
-console.log(config)
-console.log("////////////")
-console.log(process.env.NODE_ENV)
-console.log("////////////")
-console.log(process.env.DATABASE_URL)
-console.log("////////////")
 module.exports = knex(config);
