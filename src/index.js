3c3bb5cff8777363014666ded9b25aa6043aa55d
Christian cucco <cdc-96@hotmail.com>
2022-08-01 19:40:33 -0300

    correct routes and delete useless test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login';
+import Login from './pages/login/Login';
 import reportWebVitals from './reportWebVitals';
 import 'react-app-polyfill/stable';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
