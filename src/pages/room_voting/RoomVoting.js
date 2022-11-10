b08961b05abe37a22b0b9076d1b13c4c9f73b753
Christian cucco <cdc-96@hotmail.com>
2022-11-09 21:25:19 -0300

    add extra spinner

--- src/pages/room_voting/RoomVoting.js
+++ src/pages/room_voting/RoomVoting.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Col, Row, Typography} from 'antd';
 import { topMargin, buttonWidth } from '../../CommonStyles';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 
 function RoomVoting() {
@@ -33,6 +34,9 @@ function RoomVoting() {
       <Row gutter={[24, 24]} style={{ marginTop: topMargin }}>
         <Col span={24} align='middle'>
           <Title level={2}>Procesando el voto...</Title>
+        </Col>
+        <Col span={24} align='middle'>
+          <ClipLoader color={'#505050'} size={120} />
         </Col>
       </Row>  
     </div>
